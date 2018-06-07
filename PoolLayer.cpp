e56775352855972324e2b0eab9006b419a592843
Heshan Eranga <erangamx@gmail.com>
2018-06-07 20:44:08 +0530

    added PoolLayer Class

---
+++ PoolLayer.cpp
@@ -0,0 +1,78 @@
+/* 
+ * File:   PoolLayer.cpp
+ * Author: heshan
+ * 
+ * Created on June 7, 2018, 2:05 PM
+ */
+
+#include "PoolLayer.hpp"
+
+PoolLayer::PoolLayer(int depth, int height, int width, int poolW, int poolH) {
+
+    this->depth = depth;
+    this->height = height;
+    this->width = width;
+    this->poolW = poolW;
+    this->poolH = poolH;
+    this->outHeight = ((height - poolH)/poolH) + 1;
+    this->outWidth = ((width - poolW)/poolW) + 1;        
+    initMat();
+}
+
+PoolLayer::PoolLayer(const PoolLayer& orig) { }
+
+PoolLayer::~PoolLayer() { }
+
+int PoolLayer::initMat() {
+    
+    output = new Eigen::MatrixXd[depth];
+    for (int i = 0; i < depth; i++) {
+        Eigen::MatrixXd outMat = Eigen::MatrixXd::Zero(outHeight,outWidth);
+        output[i] = outMat;
+    }
+    
+    maxIndices = new Eigen::MatrixXd * [depth];
+    for (int i = 0; i < depth; i++) {
+        maxIndices[i] = new Eigen::MatrixXd[outHeight];
+        for (int j = 0; j < outHeight; j++) {
+            maxIndices[i][j] = Eigen::MatrixXd::Zero(outWidth,2);
+        }
+    }
+    return 0;
+}
+
+Eigen::MatrixXd * PoolLayer::pool(Eigen::MatrixXd * input) {
+
+    int outX, outY;
+    std::ptrdiff_t a, b;
+    for (int i = 0; i < depth; i++) {
+        outX = 0;
+        for (int x = 0; x < width; x+=poolW) {
+            outY = 0;
+            for (int y = 0; y < height; y+=poolH) {
+                Eigen::MatrixXd poolBlock = input[i /*depth*/].block(y,x,poolH,poolW);
+                output[i](outX,outY) = poolBlock.maxCoeff(&a,&b);
+                maxIndices[i][outX](outY,0) = a+y; 
+                maxIndices[i][outX](outY,1) = b+x;       
+                outY++;
+            }
+            outX++;
+        }
+    }
+
+//    for (int i = 0; i < depth; i++) {
+//        std::cout<<"\n------- Depth: "<<i<<" -------\n\n";
+//        std::cout<<output[i]<<"\n--------------\n\n";
+//    }
+//    
+//    std::cout<<"\n-------*******************************************-------\n\n";
+//    
+//    for (int i = 0; i < depth; i++) {
+//        std::cout<<"\n------- Depth: "<<i<<" -------\n\n";
+//        for (int j = 0; j < outHeight; j++) {
+//            std::cout<<maxIndices[i][j]<<"\n--------------\n\n";
+//        }
+//    }
+
+    return output;
+}
---
+++ PoolLayer.hpp
@@ -0,0 +1,55 @@
+/* 
+ * File:   PoolLayer.hpp
+ * Author: heshan
+ *
+ * Created on June 7, 2018, 2:05 PM
+ */
+
+#ifndef POOLLAYER_HPP
+#define POOLLAYER_HPP
+
+#include <iostream>
+#include <Eigen>
+
+class PoolLayer {
+public:
+    /**
+     * Initialize output matrix
+     * 
+     * @param depth: depth of the input matrix
+     * @param height: height of the input matrix
+     * @param width: width of the input matrix
+     * @param poolW: pool matrix width
+     * @param poolH: pool matrix height
+     */
+    PoolLayer(int depth, int height, int width, int poolW, int poolH);
+    /**
+     * 
+     * @param orig
+     */
+    PoolLayer(const PoolLayer& orig);
+    /**
+     * 
+     */
+    virtual ~PoolLayer();
+    /**
+     * initialize matrices
+     * @return 0
+     */
+    int initMat();
+    /**
+     * 
+     * @param input: input image or matrix
+     * @return 
+     */
+    Eigen::MatrixXd * pool(Eigen::MatrixXd * input);
+private:
+    int depth, height, width, poolW, poolH;
+    int outHeight, outWidth;
+    Eigen::MatrixXd * output;
+    Eigen::MatrixXd ** maxIndices;
+
+};
+
+#endif /* POOLLAYER_HPP */
+
